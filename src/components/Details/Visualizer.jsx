e89479c86a8c3f805a30134edbb8ab828673905c
Divij Mahajan <d.mahajan2004@gmail.com>
2023-09-02 14:55:23 +0530

    Canvas Setup

--- src/components/Details/Visualizer.jsx
+++ src/components/Details/Visualizer.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../Details/visualizer.css"
+import Canvas from "./Canvas.jsx";
 
 export default function Visualizer() {
     function insertionSort(arr, n) {
@@ -17,6 +18,21 @@ export default function Visualizer() {
             arr[j + 1] = key;
         }
     }
+    function insertionSort2(arr, n, i, j) {
+        //let i, key, j;
+        //for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        if (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            //j = j - 1;
+            setNums(() => { return [...arr] })
+            // console.log("hell")
+        }
+        arr[j + 1] = key;
+    }
+
     function shuffle(array) {
         let currentIndex = array.length, randomIndex;
         while (currentIndex > 0) {
@@ -25,20 +41,46 @@ export default function Visualizer() {
             [array[currentIndex], array[randomIndex]] = [
                 array[randomIndex], array[currentIndex]];
         }
-        return array;
+        return array
     }
     let a = [];
-    for (let i = 1; i < 25; i++) {
+    for (let i = 1; i < 30; i++) {
         a.push(i);
     }
-    shuffle(a);
+    shuffle(a)
     const [nums, setNums] = useState(a);
+    let green = "#598059";
+    let blue = "#45738C";
     //setNums(a)
+    const draw = (ctx, frameCount) => {
+        let index = 18;
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        let sorted = true;
+        for (let i = 0; i < nums.length; i++) {
+            let size = (ctx.canvas.height - 15) * nums[i] / nums.length;
+            if (i > 0 && nums[i] < nums[i - 1]) {
+                sorted = false;
+            }
+            if (i == index) {
+                ctx.fillStyle = blue;
+            } else if (sorted) {
+                ctx.fillStyle = green;
+            } else {
+                ctx.fillStyle = "aliceblue";
+            }
+
+            ctx.fillRect(20 + i * 9, ctx.canvas.height - size, 6, size);
+        }
+        if (frameCount % 4 == -1) {
+            setNums(() => { return shuffle(nums) })
+        }
+
+    }
 
     return (
         <div>
-
-            <div className="visualizer-canvas">
+            <Canvas draw={draw} className="visualizer-canvas" />
+            {/* <div className="visualizer-canvas">
                 {nums.map((n, i) => {
                     let check;
                     if (i > 0) {
@@ -49,7 +91,7 @@ export default function Visualizer() {
                     let x = check ? "var(--green)" : "white"
                     return <div key={i} style={{ height: `${18 * n / nums.length}rem`, width: "0.7rem", background: x }}></div>
                 })}
-            </div>
+            </div> */}
             <button onClick={() => { insertionSort(nums, nums.length) }}>abc</button>
         </div>
     )
