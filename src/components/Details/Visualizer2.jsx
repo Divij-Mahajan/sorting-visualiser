2daa54c18a4180fc32f8cea52a94c841b314e365
Divij Mahajan <d.mahajan2004@gmail.com>
2023-09-06 19:04:07 +0530

    pre soritng animation

---
+++ src/components/Details/Visualizer2.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "../Details/visualizer.css"
+import Canvas from "./Canvas.jsx";
+
+export default function Visualizer() {
+    function insertionSort(arr, n) {
+        let i, key, j;
+        for (i = 1; i < n; i++) {
+            key = arr[i];
+            j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                setNums(() => { return [...arr] })
+                console.log("hell")
+            }
+            arr[j + 1] = key;
+        }
+    }
+    function shuffle(array) {
+        let currentIndex = array.length, randomIndex;
+        while (currentIndex > 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+        return array
+    }
+    let a = [];
+    for (let i = 1; i < 27; i++) {
+        a.push(i);
+    }
+    shuffle(a)
+    const [nums, setNums] = useState(a);
+    let green = "#598059";
+    let blue = "#45738C";
+
+    const draw = (ctx, frameCount) => {
+        let index = 18;
+        let sorted = true;
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        for (let i = 0; i < nums.length; i++) {
+            let size = (ctx.canvas.height - 20) * nums[i] / nums.length + 4;
+            if (i > 0 && nums[i] < nums[i - 1]) {
+                sorted = false;
+            }
+            if (i == index) {
+                ctx.fillStyle = blue;
+            } else if (sorted) {
+                ctx.fillStyle = green;
+            } else {
+                ctx.fillStyle = "aliceblue";
+            }
+
+            ctx.fillRect(20 + i * 10, ctx.canvas.height - size, 6, size);
+        }
+    }
+
+
+
+    return (
+        <div>
+            <Canvas draw={draw} className="visualizer-canvas" />
+            <button onClick={() => { setNums(() => { return shuffle(nums) }) }}>abc</button>
+        </div>
+    )
+}
--- src/components/Details/useCanvas.js
+++ src/components/Details/useCanvas.js
@@ -3,18 +3,23 @@ import { useRef, useEffect } from 'react'
 const useCanvas = draw => {
 
   const canvasRef = useRef(null)
+  let delay = 1000000000000;
+  let last = 0;
+  let frameCount = 0
 
   useEffect(() => {
 
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
-    let frameCount = 0
     let animationFrameId
 
     const render = () => {
       frameCount++
       draw(context, frameCount)
-      animationFrameId = window.requestAnimationFrame(render)
+      if (frameCount - last > delay) {
+        animationFrameId = window.requestAnimationFrame(render)
+        last = frameCount
+      }
     }
     render()
 
